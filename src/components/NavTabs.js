0a81d064f5884dd2a0aeb7d303531975d9b0065f
FIRST_NAME LAST_NAME <safiamustaf>
2023-06-04 04:32:04 -0500

    About me page

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -7,11 +7,11 @@ function NavTabs({ currentPage, handlePageChange }) {
     <ul className="nav nav-tabs">
       <li className="nav-item">
         <a
-          href="#home"
-          onClick={() => handlePageChange('Home')}
+          href="#about-me"
+          onClick={() => handlePageChange('AboutMe')}
           // This is a conditional (ternary) operator that checks to see if the current page is "Home"
           // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
-          className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
+          className={currentPage === 'AboutMe' ? 'nav-link active' : 'nav-link'}
         >
           Home
         </a>
