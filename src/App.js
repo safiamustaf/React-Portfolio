58695c3a2ab4dca5eef24e4e9353bf65c3399c70
FIRST_NAME LAST_NAME <safiamustaf>
2023-06-06 11:23:14 -0500

    react

--- src/App.js
+++ src/App.js
@@ -2,5 +2,4 @@ import React from "react";
 import PortfolioContainer from "./components/PortfolioContainer";
 
 const App = () => <PortfolioContainer />;
-
-export default App;
+export default App; 
